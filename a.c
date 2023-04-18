5c91179d7072cf8c40b4a47c4ebfe83020764402
bhautik-123-rajodiya <131158263+bhautik-123-rajodiya@users.noreply.github.com>
2023-04-18 16:27:42 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+main()
+{
+//	-10...999 int  %d,%i,%u
+//	
+//	0.1...0.999 float  %f
+//	
+//	a to z char  %c
+//	
+//	name char[]
+	
+	// sum logic
+	
+	int a = 10;
+	float b = 10.20;
+	char c = 'Ravi';
+	
+	printf("a = %d\n",a);
+	printf("b = %0.0f\n",b);
+	printf("c = %c",c);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
